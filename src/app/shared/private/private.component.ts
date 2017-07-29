c968d6126386c06d2788ae2a33d1c56408d5fb49
DavidLevayer <david.levayer@gmail.com>
2017-07-29 19:07:21 +0200

    Add zone name in breadcrumbs

--- src/app/shared/private/private.component.ts
+++ src/app/shared/private/private.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ZoneData } from '../zone-data';
 import { ZonesListService } from '../zones-list.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,14 +12,27 @@ import { Component, OnInit } from '@angular/core';
 export class PrivateComponent implements OnInit {
 
     zones: ZoneData[];
+    currentZoneName: string;
     error: Error;
 
-    constructor(private zoneListService: ZonesListService) { }
+    constructor(private zoneListService: ZonesListService, private route: ActivatedRoute, private router: Router) { }
 
     ngOnInit() {
         this.zoneListService.getZones().subscribe(
             res => this.zones = res,
             err => this.error = err
-        )
+        );
+
+        // Init zone name with current route (if needed)
+        this.route.firstChild.params.subscribe((params: { name: string }) => {
+            this.currentZoneName = params.name;
+        });
+
+        // Update zone name each time the router is used
+        this.router.events
+            .filter(event => event instanceof NavigationEnd)
+            .subscribe((event: NavigationEnd) => {
+                this.currentZoneName = event.url.replace('/zones', '').replace('/', '');
+            });
     }
 }
