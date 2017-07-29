49d28bfb0a05fb445b0dba8f0be0c0db4746177e
DavidLevayer <david.levayer@gmail.com>
2017-07-29 16:00:31 +0200

    Router refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,32 @@
-// vim: set tabstop=2 expandtab filetype=javascript:
+import { PrivateComponent } from './shared/private/private.component';
+import { ZonesDetailComponent } from './zones/zones-detail/zones-detail.component';
+import { ZonesListComponent } from './zones/zones-list/zones-list.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-//import { ZonesListComponent } from './zones/zones-list/zones-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 import { LoginComponent } from './login/login.component';
 import { CheckAuthGuard } from './check-auth/check-auth.guard';
 
+const privateRoutes: Routes = [
+	{ path: '', redirectTo: 'zones', pathMatch: 'full' },
+	{ path: 'zones', component: ZonesListComponent },
+	{ path: 'zones/:name', component: ZonesDetailComponent },
+	{ path: '**', component: PageNotFoundComponent }
+];
+
 const appRoutes: Routes = [
-	{
-		path: '',
-		redirectTo: '/zones',
-		pathMatch: 'full'
-	},
-	{
-		path: 'login',
-		component: LoginComponent
-	},
-	{
-		path: '**',
-		component: PageNotFoundComponent,
-		canActivate: [CheckAuthGuard]
-	}
+	{ path: 'login', component: LoginComponent },
+	{ path: '', component: PrivateComponent, children: privateRoutes }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoutes)
-  ],
-  exports: [
-    RouterModule
-  ]
+	imports: [
+		RouterModule.forRoot(appRoutes)
+	],
+	exports: [
+		RouterModule
+	]
 })
 export class AppRoutingModule { }
