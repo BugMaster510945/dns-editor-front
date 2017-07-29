ce12c1d2ae0dd3074caccb787c5e5b2dd94ea42b
DavidLevayer <david.levayer@gmail.com>
2017-07-29 17:39:53 +0200

    Init breadcrumbs

--- src/app/shared/private/private.component.ts
+++ src/app/shared/private/private.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ZoneData } from '../zone-data';
+import { ZoneListService } from '../zones-list.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-private',
@@ -6,6 +8,17 @@ import { Component } from '@angular/core';
     styleUrls: ['private.component.scss']
 })
 
-export class PrivateComponent {
-    constructor() { }
+export class PrivateComponent implements OnInit {
+
+    zones: ZoneData[];
+    error: Error;
+
+    constructor(private zoneListService: ZoneListService) { }
+
+    ngOnInit() {
+        this.zoneListService.getZones().subscribe(
+            res => this.zones = res,
+            err => this.error = err
+        )
+    }
 }
