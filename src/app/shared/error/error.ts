ec96bcdd35b08cfa945b5906f22c5eac60f717b6
Frédéric Planchon <github@planchon.org>
2017-08-27 17:42:57 +0200

    Adaptation pour les erreurs

--- src/app/shared/error/error.ts
+++ src/app/shared/error/error.ts
@@ -1,12 +1,21 @@
 // vim: set tabstop=2 expandtab filetype=javascript:
 export class Error {
 
-    status: number;
-    message: string;
-    details: string;
+  status: number;
+  message: string;
+  details: Array<String>;
+  filename: string;
+  fileline: number;
 
-    constructor(status: number, message: string, details?: string){
-        this.status = status;
-        this.message = message;
-    }
+  constructor(status: number, message: string, obj?: any)
+  {
+    this.status = status;
+    this.message = obj && obj.message || message;
+    this.details = obj && obj.details || [];
+    this.filename = obj && obj.filename || '';
+    this.fileline = obj && obj.fileline || 0;
+
+    // ignore stacktrace
+  }
+
 }
