a128fdc1f4c89a9508adde13d175b85d5acd18ea
Frédéric Planchon <github@planchon.org>
2017-09-12 12:32:56 +0200

    Debut internationalisation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
+// vim: set tabstop=2 expandtab filetype=javascript:
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+//import { getTranslationProviders } from './app/shared/i18n-providers';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
+/*getTranslationProviders().then(providers => {
+  const options = { providers };
+  platformBrowserDynamic().bootstrapModule(AppModule, options);
+});
+*/
 platformBrowserDynamic().bootstrapModule(AppModule);
