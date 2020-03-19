69226e5d961fb456dbb70a23e6d51a1ee635b214
Frédéric Planchon <github@planchon.org>
2020-03-19 15:43:58 +0100

    correction localize + guard

--- src/app/common/check-auth.guard.ts
+++ src/app/common/check-auth.guard.ts
@@ -1,22 +1,29 @@
 // vim: set tabstop=2 expandtab filetype=javascript:
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '@app/common/auth.service';
 
 @Injectable()
-export class CheckAuthGuard implements CanActivate {
+export class CheckAuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
     _: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-    if (this.authService.checkCredentials()) {
+    if (!this.authService.isValidConnectedUser()) {
       this.authService.saveRedirectURL(state.url);
 
       this.router.navigate(['/login']);
+      return false;
     }
     return true;
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    return this.canActivate(route, state);
   }
 }
